eb8d5f2487a3e2502554cd9a1a407c0e41788ab3
extrovert <349173537@qq.com>
2023-01-20 20:35:33 +0800

    dateFormat_conditional

---
+++ BBQ/node/dateFormat.js
@@ -0,0 +1,24 @@
+// 格式化时间传统方法
+function dateFormat(dtStr) {
+  const dt = new Date(dtStr)
+
+  const y = dt.getFullYear()
+  const m = padZero(dt.getMonth() + 1) //month获取的是0-11
+  const d = padZero(dt.getDay())
+
+
+  const hh = padZero(dt.getHours())
+  const mm = padZero(dt.getMinutes())
+  const ss = padZero(dt.getSeconds())
+
+  return `${y} - ${m} - ${d} - ${hh} - ${mm} - ${ss}`
+
+}
+function padZero(n) {
+  return n > 9? n : '0' + n
+}
+
+
+module.exports = {
+  dateFormat: dateFormat()
+}
