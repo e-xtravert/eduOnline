d1bd2e8561f6a4d5c19eda1411411a9e3220a56b
extrovert <349173537@qq.com>
2023-01-23 23:49:15 +0800

    api_cors

--- BBQ/node/api/use_api.js
+++ BBQ/node/api/use_api.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 
@@ -6,6 +7,7 @@ const app = express()
 app.use(express.json())
 // app.use(express.urlencoded({extended: false}))
 
+app.use(cors())
 const router = require('./apiRouter')
 //这个/api是跟路径？
 app.use('/api', router.router)
