22769acda2aed2936cd9d44e1b5da0fe7cd535b6
extravert <349173537@qq.com>
2023-02-06 14:40:14 +0800

    nothing

--- BBQ/python_base/dp_about/package.py
+++ BBQ/python_base/dp_about/package.py
@@ -3,51 +3,34 @@
 牛客dp41，背包问题
 '''
 
-
-
-while True:
-    try:
-        n, V = map(int, input().split())
-        lv, lw = [], []
-        for i in range(n):
-            l = list(map(int, input().split()))
-            lv.append(l[0])  # 物品重量列表
-            lw.append(l[1])  # 物品价值列表
-        # 2 双结果dp[j]
-        dp = [0 for _ in range(V + 1)]
-        # dp[j]表示背包容量为j时装满背包的最大价值。
-        # 最终输出时，问题1的答案是整个dp的最大值，问题2的答案是dp的最后一个数，初始化时数字是0所以最后如果的不到结果也满足答案要求。
-        for i in range(n):
-            for j in range(V, -1, -1):  # 从后向前算，防止数据混淆
-                if lv[i] <= V and(j == lv[i] or (dp[j - lv[i]] != 0 and j - lv[i] > 0)):
-                    # 条件首先剔除无法放入背包的物品。然后如果新加入的物品可以单独放入某一个j、或者与已存在的值可以组成j的值进行计算。
-                    #  由于Python的特性dp需剔除负坐标j - lv[i] > 0
-                    dp[j] = max(dp[j], dp[j - lv[i]] + lw[i])
-                    # dp[j]的值只与其当前值、能与lv[i]重量组合成j重量的dp[j - lv[i]]，两个数中的较大值
-        print(max(dp))
-        print(dp[-1])
-    except:
-        break
-
-
-
-
 n, V = map(int, input().split())
 # map1 = [ [ *map(int, input().split())] for _ in range(n)]
-v = []
-w = []
+v, w = [], []
 for i in range(n):
     lis = list(map(int, input().split()))
     v.append(lis[1])
     w.append(lis[0])
 
-dp = [0] * (n + 1)
+# dp = [[0] * (V + 1) for i in range(n)]
+dp0 = [0 for i in range(V + 1)]
+dp1 = [float('-inf') for _ in range(V + 1)]
+dp1[0] = 0
 for i in range(n):
-    if V - v[i] >= 0:
-        dp[i] = max(dp[i - 1], dp[V - v[i]] + w[i])
+    for j in range(V, w[i] - 1, -1):
+        dp0[j] = max(dp0[j], dp0[j - w[i]] + v[i])
+        dp1[j] = max(dp1[j], dp1[j - w[i]] + v[i])
 
-
+print(dp0[-1])
+print(dp1[-1] if dp1[-1] != float('-inf') else 0)
 # dp[i] = max(dp[i - 1], dp[V - v[i]] + w[i])，当背包恰好装满就多一个i <= 容量的条件就行了呗？
+# 为什么这样不行，因为i原本只表示状态，不表示其他含义，上面这样写需要复杂一点操作，还需要对V-v[i]进行判断，不如用下面的dp,表示第i个物品放与不放和背包容量为j时的状态
+# dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]),
+# 选dp数组，初始化，双循环设置,一个遍历背包，一个遍历物品
 
-
-print(n, V, v, w, dp)
+# for i in range(n):
+#     for j in range(V, -1, -1):
+#         if j - w[i] >= 0:
+#             dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
+#         else:
+#             dp[i][j] = dp[i - 1][j]
+# print(n, V, v, w, dp0, dp1)
