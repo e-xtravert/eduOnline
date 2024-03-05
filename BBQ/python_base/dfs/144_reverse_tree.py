ade51d287cf57c9275d2c2bf275b34afc3b926c6
extravert <349173537@qq.com>
2024-03-05 21:06:22 +0800

    dfs

--- BBQ/python_base/dfs/144_reverse_tree.py
+++ BBQ/python_base/dfs/144_reverse_tree.py
@@ -1,3 +1,18 @@
 # @Time    : 2024/3/5 17:39
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution:
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return
+
+        # 这个翻转真是不好理解呀 当他递归到最深的时候为叶子节点返回了， 和左子节点交换 交换完
+        root.left, root.right = self.mirrorTree(root.right), self.mirrorTree(root.left)
+        # 然后这里把上面的交换之后的root返回去了到上一层去
+        return root
 
--- BBQ/python_base/dfs/lcr145.py
+++ BBQ/python_base/dfs/lcr145.py
@@ -1,3 +1,29 @@
 # @Time    : 2024/3/5 21:00
+# 判断对称二叉树
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def checkSymmetricTree(self, root: Optional[TreeNode]) -> bool:
+        # if not root:
+        #     return True
 
+        # 这样其实是判断两个子树是否相同 并没有对称
+        # elif root.left != self.checkSymmetricTree(root.right) or \
+        #    root.right != self.checkSymmetricTree(root.left):
+        #    return False
 
+        # return root
+
+        def recur(l, r):
+            # 其实前面的 都算终止条件 最后的是操作
+            if not l and not r:
+                return True
+            if not l or not r or l.val != r.val:
+                return False
+            return recur(l.left, r.right) and recur(l.right, r.left)
+
+        return not root or recur(root.left, root.right)
